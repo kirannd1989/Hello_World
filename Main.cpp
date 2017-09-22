04f12e2e599630486b4aae7d9e64aa6ed14d49d1
kirannd1989 <32192060+kirannd1989@users.noreply.github.com>
2017-09-22 09:33:51 +0100

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef unsigned int intu;
+typedef int ints;
+typedef unsigned char charu;
+typedef char chars;
+
+#define boost ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define rep(i, n)  for(int i = 0; i < (n); ++i)
+#define rep1(i, n) for(int i = 1; i <= (n); ++i)
+
+
+
+	
+int main(){
+freopen("input.txt", "rb", stdin);
+
+
+
+
+return 0;
+
+}
